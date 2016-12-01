b1b533debbc82089ecc0bb23cb7929ea942e1fea
ferjordo <ferjordo2011@gmail.com>
2016-12-01 00:38:24 -0500

    Creacion de las 3 primeras clases

---
+++ PROYECTO/PROYECTO/Administrador.vb
@@ -0,0 +1,31 @@
+﻿Public Class Administrador
+    Inherits Persona
+    Private usuarioAdministrador As String
+    Public Property Usuario_ADMIN() As String
+        Get
+            Return usuarioAdministrador
+        End Get
+        Set(ByVal value As String)
+            usuarioAdministrador = value
+        End Set
+    End Property
+
+    Private passwordAdmin As String
+    Public Property Password_ADMIN() As String
+        Get
+            Return passwordAdmin
+        End Get
+        Set(ByVal value As String)
+            passwordAdmin = value
+        End Set
+    End Property
+
+    Sub New(ByVal nombre As String, ByVal apellido As String, ByVal cedula As String, ByVal user As String, ByVal pass As String)
+        Me.Nombre = nombre
+        Me.Apellido = apellido
+        Me.Cedula = cedula
+        Me.usuarioAdministrador = user
+        Me.Password_ADMIN = pass
+    End Sub
+
+End Class
---
+++ PROYECTO/PROYECTO/Votante.vb
@@ -0,0 +1,12 @@
+﻿Public Class Votante
+    Inherits Persona
+
+    Sub New(ByVal nombre As String, ByVal apellido As String, ByVal cedula As String)
+        Me.Nombre = nombre
+        Me.Apellido = apellido
+        Me.Cedula = cedula
+
+    End Sub
+
+
+End Class
