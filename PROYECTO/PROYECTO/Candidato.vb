e74b6b63d233b2c0a2b8caedbc8a76f16ad69eec
majocabr <majocabr.93@gmail.com>
2016-12-01 01:12:26 -0500

    clases mesa y candidato

---
+++ PROYECTO/PROYECTO/Candidato.vb
@@ -0,0 +1,60 @@
+﻿Public Class Candidato
+    Inherits Persona
+    Private _usuarioCandidato As String
+    Public Property Usuario_ADMIN() As String
+        Get
+            Return _usuarioCandidato
+        End Get
+        Set(ByVal value As String)
+            _usuarioCandidato = value
+        End Set
+    End Property
+
+    Private _passwordCandidato As String
+    Public Property Password_ADMIN() As String
+        Get
+            Return _passwordCandidato
+        End Get
+        Set(ByVal value As String)
+            _passwordCandidato = value
+        End Set
+    End Property
+    Private _dignidad As String
+    Public Property Dignidad() As String
+        Get
+            Return _dignidad
+        End Get
+        Set(ByVal value As String)
+            _dignidad = value
+        End Set
+    End Property
+    Private _partido As String
+    Public Property Partido() As String
+        Get
+            Return _partido
+        End Get
+        Set(ByVal value As String)
+            _partido = value
+        End Set
+    End Property
+    Private _votos As Integer
+    Public Property Votos() As Integer
+        Get
+            Return _votos
+        End Get
+        Set(ByVal value As Integer)
+            _votos = value
+        End Set
+    End Property
+
+    Sub New(ByVal nombre As String, ByVal apellido As String, ByVal cedula As String, ByVal user As String, ByVal pass As String, ByVal dig As String, ByVal part As String)
+        Me.Nombre = nombre
+        Me.Apellido = apellido
+        Me.Cedula = cedula
+        Me._usuarioCandidato = user
+        Me._passwordCandidato = pass
+        Me._dignidad = dig
+        Me._partido = part
+        Me._votos = 0
+    End Sub
+End Class
---
+++ PROYECTO/PROYECTO/MesaElectoral.vb
@@ -0,0 +1,35 @@
+﻿Public Class MesaElectoral
+
+    Private _administrador As Administrador
+    Public Property Admin() As Administrador
+        Get
+            Return _administrador
+        End Get
+        Set(ByVal value As Administrador)
+            _administrador = value
+        End Set
+    End Property
+
+    Private _ubicacion As String
+    Public Property Ubicacion() As String
+        Get
+            Return _ubicacion
+        End Get
+        Set(ByVal value As String)
+            _ubicacion = value
+        End Set
+    End Property
+
+    Private _candidatos As ArrayList
+
+    Private _listaVotantes As ArrayList
+    Private _vocales As ArrayList
+    Sub New(ByVal admin As Administrador, ByVal ubi As String)
+        Me._administrador = admin
+        Me._ubicacion = ubi
+        Me._listaVotantes = New ArrayList()
+        Me._vocales = New ArrayList()
+        Me._candidatos = New ArrayList()
+
+    End Sub
+End Class
